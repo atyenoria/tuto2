4e95f9a5e067be2b861bd52ce8972e6b19b3bc91
atyenoria <eigotyoubunn30@gmail.com>
2015-05-08 23:47:29 +0900

    a

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -40,5 +40,5 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  ActiveModel::SecurePassword.min_cost = true
+  # ActiveModel::SecurePassword.min_cost = true
 end
